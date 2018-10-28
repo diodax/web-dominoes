a102c00e3310f5426cba4878ef86a440e533354c
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2018-10-28 01:57:46 -0400

    #6 Implement basic /login functionality

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,63 +1,6 @@
-<footer role="contentinfo" class="py-6 lh-1 bg-white">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-2">
-              <h3 class="h4 mb-4">Robust.</h3>
-            </div>
-            <div class="col-md-10">
-              <div class="row">
-                <div class="col-md-3 col-sm-6">
-                  <h4 class="h6">Address</h4>
-                  <address>
-                    <ul class="list-unstyled">
-                      <li>
-                        City Hall<br>
-                        212  Street<br>
-                        Lawoma<br>
-                        735<br>
-                      </li>
-                    </ul>
-                  </address>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                  <h4 class="h6">Popular Services</h4>
-                  <ul class="list-unstyled">
-                    <li><a href="#">Payment Center</a></li>
-                    <li><a href="#">Contact Directory</a></li>
-                    <li><a href="#">Forms</a></li>
-                    <li><a href="#">News and Updates</a></li>
-                    <li><a href="#">FAQs</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                  <h4 class="h6">Website Information</h4>
-                  <ul class="list-unstyled">
-                    <li><a href="#">Website Tutorial</a></li>
-                    <li><a href="#">Accessibility</a></li>
-                    <li><a href="#">Disclaimer</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Webmaster</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                  <h4 class="h6">Company</h4>
-                  <ul class="list-unstyled">
-                    <li><a href="#">Our team</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="https://themes.getbootstrap.com/product/robust-ui-kit-dashboard-landing/" target="_blank">Purchase</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-12 text-center text-sm">
-              <p class="mb-0">© 2018 - <a href="index.html">Robust UI Kit</a>.</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+<footer role="contentinfo" class="py-3 lh-1 bg-white">
+    <div class="text-center text-sm">
+        <p class="mb-0">© 2018 - <a href="https://diodax.github.io/">Diosdavi Lara</a>.</p>
+        <p>The source code can be found on <a href="https://github.com/diodax/web-dominoes">GitHub</a></p>
+    </div>
+</footer>
